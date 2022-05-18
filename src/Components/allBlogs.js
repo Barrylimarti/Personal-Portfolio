dbde53562971e05c7cea98d5889b5327863970f0
RST <rahulsthakur19@gmail.com>
2022-05-18 23:25:19 +0530

    Added experience

--- src/Components/allBlogs.js
+++ src/Components/allBlogs.js
@@ -1,9 +1,33 @@
 import blog1 from "../img/blogs/Flearn.JPG";
 import blog2 from "../img/blogs/Keyworks2.JPG";
+import blog3 from "../img/blogs/ethanLogo.png";
+import blog4 from "../img/blogs/IBYLogo.png";
 
 const blogs = [
   {
     id: 1,
+    image: blog4,
+    title: "SDE Intern",
+    subtitle: "I'm Beside You, Tokyo, Japan",
+    link: "Experience1",
+    para: "Currently working.....",
+    date: "21",
+    month: "Mar",
+    year: "2020",
+  },
+  {
+    id: 2,
+    image: blog3,
+    title: "SDE Intern",
+    subtitle: "Ethan AI, Singapore",
+    link: "Experience1",
+    para: "Worked as a React developer. Built the frontend for Ethan RM Dashboard with React/Redux and Ethan mobile application with React-native. Implemented several APIs integration with live fetching of data.",
+    date: "21",
+    month: "Mar",
+    year: "2020",
+  },
+  {
+    id: 3,
     image: blog2,
     title: "SDE Intern",
     subtitle: "Keyworks Solution, Hyderabad",
@@ -14,7 +38,7 @@ const blogs = [
     year: "2020",
   },
   {
-    id: 2,
+    id: 4,
     image: blog1,
     title: "Full Stack Developer",
     subtitle: "Flearn Technologies, Gurugram",
